bc4f04121356b2653eae71f71efabf6c445ae648
Andy Bird <andyb@Andys-Mac-mini.local>
2022-06-16 15:00:52 +0100

    Refactored the deposit and withdraw methods within account class to incorporate SRP and keep the code clean. All tests still passing

--- lib/account.rb
+++ lib/account.rb
@@ -2,7 +2,7 @@ require './lib/statement'
 
 class Account
 
-    attr_reader :balance, :statement
+    attr_reader :balance, :statement, :transactions
 
 def initialize(statement = Statement.new, transaction = Transaction)
     @transactions = []
@@ -12,11 +12,20 @@ def initialize(statement = Statement.new, transaction = Transaction)
 end
 
 def deposit(amount)
-    @balance += amount
+    increment_balance(amount)
 end
 
 def withdraw(amount)
+    decrement_balance(amount)
+end
+
+def increment_balance(amount)
+    @balance += amount
+end
+
+def decrement_balance(amount)
     @balance -= amount
 end
+
 
 end
