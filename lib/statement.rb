94a5391c3c073ee4ef574fe204cccde33ad3b596
Andy Bird <andyb@Andys-Mac-mini.local>
2022-06-16 14:12:52 +0100

    added headers to statement class and added test to ensure headers appear correctly on all statements

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,3 +1,9 @@
+require './lib/account'
+
 class Statement
+    
+    def title
+      'date || credit || debit || balance'
+    end
 
 end
---
+++ spec/statement_spec.rb
@@ -0,0 +1,8 @@
+require './lib/statement'
+
+describe Statement do
+    it 'expects statement to have title/headers' do
+        test_statement = Statement.new
+        expect(test_statement.title).to eq('date || credit || debit || balance')
+    end
+end
